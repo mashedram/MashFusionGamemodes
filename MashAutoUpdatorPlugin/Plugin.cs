6caaa4d4e967e962c766c07935ce2a529b4afd65
mash <basbaskker1912@gmail.com>
2025-10-06 13:37:10 +0200

    Added the project

---
+++ MashAutoUpdatorPlugin/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Resources;
+using MashAutoUpdatorPlugin;
+using MelonLoader;
+
+[assembly: AssemblyTitle(Plugin.Name)]
+[assembly: AssemblyProduct(Plugin.Name)]
+[assembly: AssemblyCopyright("Created by " + Plugin.Author)]
+[assembly: AssemblyVersion(Plugin.Version)]
+[assembly: AssemblyFileVersion(Plugin.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(Plugin), Plugin.Name, Plugin.Version, Plugin.Author, null)]
+
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+namespace MashAutoUpdatorPlugin;
+
+public class Plugin : MelonPlugin
+{
+    public const string Name = "Mash Auto Updator";
+    public const string Author = "Mash";
+    public const string Version = "1.0.0";
+    
+    // TODO: Make these stored in a file
+    public const string RemoteURL = "http://localhost:8000/";
+    public const string Repo = "clockhunt";
+    public const string Channel = "release";
+
+    public override void OnPreInitialization()
+    {
+        
+    }
+}
