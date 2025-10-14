6a8bb93dea03ece39e1f12fb6e67a92ed82d49d0
mash <mash@mashedram.dev>
2025-10-14 14:54:07 +0200

    Initial Commit

---
+++ BoneStrike/FusionModule.cs
@@ -0,0 +1,6 @@
+﻿namespace BoneStrike;
+
+public class FusionModule
+{
+    
+}
---
+++ BoneStrike/Mod.cs
@@ -0,0 +1,11 @@
+﻿
+using BoneStrike;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Mod), "Bonestrike", "0.1.0", "Mash")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+namespace BoneStrike;
+
+public class Mod : MelonMod
+{
+}
