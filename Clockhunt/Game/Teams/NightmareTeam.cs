32e11774ae411a1e53ca14f235e99259c8de9b16
mash <basbaskker1912@gmail.com>
2025-10-16 12:20:14 +0200

    Implement INetSerializable for NightmareTeam and add nightmare ID management

--- Clockhunt/Game/Teams/NightmareTeam.cs
+++ Clockhunt/Game/Teams/NightmareTeam.cs
@@ -1,21 +1,32 @@
 ﻿using Clockhunt.Config;
 using Clockhunt.Nightmare;
+using LabFusion.Network.Serialization;
 using LabFusion.Player;
 using MashGamemodeLibrary.Execution;
 using MashGamemodeLibrary.Player.Team;
+using MelonLoader;
 
 namespace Clockhunt.Game.Teams;
 
-public class NightmareTeam : Team
+public class NightmareTeam : Team, INetSerializable
 {
     public override string Name => "Nightmare";
     public override uint Capacity => 1;
+
+    private int _nightmareID;
+    public int NightmareID => _nightmareID;
+
+    public NightmareTeam(int nightmareID)
+    {
+        _nightmareID = nightmareID;
+    }
 
     public override void OnAssigned(PlayerID player)
     {
         Executor.RunIfHost(() =>
         {
             NightmareManager.SetRandomNightmare(player);
+            MelonLogger.Msg("Assigned nightmare to player " + player + " with nightmare ID " + NightmareID);
         });
     }
 
@@ -25,5 +36,10 @@ public class NightmareTeam : Team
         {
             NightmareManager.RemoveNightmare(player);
         });
+    }
+
+    public void Serialize(INetSerializer serializer)
+    {
+        serializer.SerializeValue(ref _nightmareID);
     }
 }
